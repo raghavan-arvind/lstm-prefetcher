c1c5ff77bc6e7e20c557e000b1e9801ccc770173
Arvind Raghavan <raghavan@cs.utexas.edu>
2018-04-26 13:50:54 -0500

    removed preprocessing one hot encoding inputs

--- embedding.py
+++ embedding.py
@@ -10,15 +10,24 @@ def debug(message):
         sys.stdout.write(message)
         sys.stdout.flush()
 
-def one_hot_encode(deltas):
+# one hot encode a data set
+def one_hot_encode(data):
     encode = dict()
-    decode = dict()
+    #decode = dict()
 
-    deltas = list(deltas)
-    for i, d in enumerate(deltas):
+    deltas = list(data)
+    for i, d in enumerate(data):
         encode[d] = i
-        decode[i] = d
+        #decode[i] = d
     return encode #, decode
+
+# used because unknown encodings should
+# all go to the same index
+def enc(encoder, element):
+    if element in encoder:
+        return encoder[element]
+    else:
+        return len(encoder)
 
 # crawls the trace and returns a dictionary representing 
 # the frequency of each delta
@@ -80,30 +89,29 @@ def crawl_trace(filename, input_deltas, output_deltas, pcs, time_steps, limit=-1
                 addr, pc = [int(s) for s in line.strip().split()]
                 if prev != -1:
                     delta = addr - prev
-                    cur_trace.append((delta, pc, delta in input_enc))
+                    cur_trace.append((delta, pc))
                     
+                    # still using a sliding window because its the most
+                    # efficient way to get sets of time_steps accesses
                     if len(cur_trace) == time_steps+1:
+                        # no preprocessing anymore, invalid inputs map to
+                        # the same encoding
+                        '''
                         # check if all but the last are valid
                         input_valid = all(c[2] for c in cur_trace[:-1])
                         # check if last is valid
                         output_valid = cur_trace[time_steps][0] in output_enc
+                        '''
 
-                        if input_valid and output_valid:
-                            for step in cur_trace[:-1]:
-                                delta_enc = [False for i in range(len(input_enc))]
-                                delta_enc[input_enc[step[0]]] = True
+                        for step in cur_trace[:-1]:
+                            delta_index = enc(input_enc, step[0])
+                            pc_index = enc(pcs_enc, step[1])
+                            trace_in.append((delta_index, pc_index))
+                        
+                        output_step = cur_trace[time_steps]
+                        output_index = enc(output_enc, output_step[0])
 
-                                pc_enc = [False for i in range(len(pcs_enc))]
-                                pc_enc[pcs_enc[step[1]]] = True
-
-                                delta_enc.extend(pc_enc)
-                                trace_in.append(delta_enc)
-                            
-                            output_step = cur_trace[time_steps]
-                            output_step_enc = [False for i in range(len(output_enc))]
-                            output_step_enc[output_enc[output_step[0]]] = True
-
-                            trace_out.append(output_step_enc)
+                        trace_out.append(output_index)
 
                         del cur_trace[0]
                     count += 1
@@ -114,17 +122,7 @@ def crawl_trace(filename, input_deltas, output_deltas, pcs, time_steps, limit=-1
     return trace_in, trace_out
 
 
-
-def get_embeddings(filename, time_steps, train_ratio=0.70, lim=-1):
-    deltas, pcs = crawl_deltas(filename, limit=lim)
-
-    input_deltas = sorted([x for x in deltas.keys() if deltas[x] >= 10], key=lambda x: deltas[x], reverse=True)
-    size = min(50000, len(deltas.keys()))
-    output_deltas = sorted(deltas.keys(), key=lambda x: deltas[x], reverse=True)[:size]
-
-    trace_in, trace_out = crawl_trace(filename, input_deltas, output_deltas, pcs, time_steps, limit=lim)
-    debug("Created " + str(len(trace_out)) + " training sets!\n")
-
+def split_training(trace_in, trace_out, time_steps, train_ratio=0.70):
     cutoff_y = int(train_ratio*len(trace_out))
     cutoff_x = cutoff_y * time_steps
 
@@ -136,7 +134,25 @@ def get_embeddings(filename, time_steps, train_ratio=0.70, lim=-1):
 
     return np.array(train_x), np.array(train_y), np.array(test_x), np.array(test_y)
 
+def get_embeddings(filename, time_steps, train_ratio=0.70, lim=-1):
+    deltas, pcs = crawl_deltas(filename, limit=lim)
+
+    input_deltas = sorted([x for x in deltas.keys() if deltas[x] >= 10], key=lambda x: deltas[x], reverse=True)
+    size = min(50000, len(deltas.keys()))
+    output_deltas = sorted(deltas.keys(), key=lambda x: deltas[x], reverse=True)[:size]
+
+    trace_in, trace_out = crawl_trace(filename, input_deltas, output_deltas, pcs, time_steps, limit=lim)
+    debug("Created " + str(len(trace_out)) + " sets!\n")
+
+    # ungodly return statement, but what can you do....
+    return np.array(trace_in), np.array(trace_out), len(input_deltas)+1, len(pcs)+1, len(output_deltas)+1
+
 
 if __name__ == '__main__':
-    get_embeddings(sys.argv[1], 20)
+    trace_in, trace_out, num_inputs, num_pcs, num_output = get_embeddings(sys.argv[1], 20)
+    print(trace_in)
+    print("\n")
+    print(trace_out)
 
+    print(len(trace_in))
+    print(len(trace_out))
--- lstm.py
+++ lstm.py
@@ -1,14 +1,14 @@
 import sys, os
 import tensorflow as tf
 import tensorflow.contrib.rnn as rnn
-from embedding import get_embeddings
+from embedding import get_embeddings, split_training
 
 
 # number of instructions per prediction
-time_steps = 20
+time_steps = 64
 
 # number of nodes in the LSTM
-num_units = 50
+num_units = 128
 
 # learning rate
 learning_rate = 0.001
@@ -20,13 +20,22 @@ batch_size = 128
 train_ratio = 0.70
 
 # get datasets
-train_x, train_y, test_x, test_y = get_embeddings(sys.argv[1], time_steps, train_ratio=train_ratio, lim=-1)
+trace_in, trace_out, n_input_deltas, n_pcs, n_output_deltas  = get_embeddings(sys.argv[1], time_steps)
 
 # number of inputs to LSTM
-n_input = len(train_x[0]) # num of unique PCS + num of deltas w/ 10 or more accesses
+n_input = 2
 
-# number of deltas
-n_classes = len(train_y[0]) # should be 50000 or less for small trace
+# number of output deltas
+n_classes = n_output_deltas 
+assert n_classes <= 50001, "%d > 50001 output classes!" % n_classes
+
+
+# split into training sets
+train_x, train_y, test_x, test_y = split_training(trace_in, trace_out, time_steps, train_ratio=train_ratio)
+
+# clear up space
+del trace_in
+del trace_out
 
 # weights and biases of appropriate shape
 out_weights = tf.Variable(tf.random_normal([num_units, n_classes]))
@@ -40,7 +49,8 @@ None - means don't enforce batch_size, bc we use
 # input instruction placeholder
 x = tf.placeholder("float", [None, time_steps, n_input]) # None means t
 # input label placeholder
-y = tf.placeholder("int32", [None, n_classes])
+y = tf.placeholder("int64", [None, 1]) # takes it in as 1 dimensional
+y_one_hot = tf.one_hot(y, n_classes)
 
 # process input tensor to list [batch_size, n_inputs] of length time_steps
 inputs = tf.unstack(x, time_steps, 1);
@@ -54,7 +64,7 @@ prediction = tf.matmul(outputs[-1], out_weights) + out_bias
 
 # loss function
 loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-    logits=prediction, labels=y))
+    logits=prediction, labels=y_one_hot))
 # optimization
 opt = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
@@ -78,7 +88,9 @@ with tf.Session() as sess:
         batch_x = train_x[x_range[0]:x_range[1]]
         batch_x = batch_x.reshape((batch_size * time_steps * n_input))
         batch_x = batch_x.reshape((batch_size, time_steps, n_input))
+
         batch_y = train_y[iterator*batch_size:(iterator+1)*batch_size]
+        batch_y = batch_y.reshape((batch_size, 1))
 
         sess.run(opt, feed_dict={x: batch_x, y: batch_y})
 
@@ -102,5 +114,5 @@ with tf.Session() as sess:
 
     # evaluate on second half of data set
     test_data = test_x.reshape((-1, time_steps, n_input)) # -1 means make batch size the whole data set
-    test_label = test_y
+    test_label = test_y.reshape((-1, 1))
     print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: test_data, y: test_label}))
